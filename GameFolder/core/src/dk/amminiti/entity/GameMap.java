1103cf378cecd6bd621370c3a5860a32588b3060
Christopher Buchardt <christopher.buchardt@gmail.com>
2018-11-16 19:59:05 +0100

    Cleaned up player

---
+++ GameFolder/core/src/dk/amminiti/entity/GameMap.java
@@ -0,0 +1,4 @@
+package dk.amminiti.entity;
+
+public class GameMap {
+}
--- GameFolder/core/src/dk/amminiti/entity/Player.java
+++ GameFolder/core/src/dk/amminiti/entity/Player.java
@@ -1,17 +1,13 @@
 package dk.amminiti.entity;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
-import com.badlogic.gdx.audio.Music;
-import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.physics.box2d.*;
-import java.util.ArrayList;
-import java.util.Collections;
+import dk.amminiti.InputController;
+
 
 
 public class Player extends TextureObject {
@@ -46,108 +42,50 @@ public class Player extends TextureObject {
     private World world;
     private int lookingDir = 1;
     private boolean isMidAir = false;
-    private boolean isCrouched = false;
     private boolean hasJumped = false;
-    private PlayerAnimationController animController;
-    private Vector2 placeVector = new Vector2(0,0);
-    private Vector2 gunPosition = new Vector2(0, 0);
+//    private PlayerAnimationController animController;
 
-    private boolean isPodLanding = true;
     private boolean isDead = false;
-    private float deathTimer = 0;
     private float timePassed = 0;
     private int deathCounter;
-    private Vector2 podPosition;
     private Vector2 spawnPosition;
     private GameMap map;
-
-    private Sound placementSound;
-    private Sound deathSound;
-    private Sound qCollectSound;
-    private Sound bounceSound;
-    private Music walkingOnIce;
-    private Sound walkingSound;
-    private Sound pickupSound;
-    private Sound voidSound;
-
-
-    private boolean onIce;
-
-
     private Body feet;
+
 
     public Player(World world, Vector2 pos) {
         super(world, pos, createPlayerBodyDef(), createPlayerFixtureDef(), null);
-        animController = new PlayerAnimationController(this, playerTexture);
-
-
+        //animController = new PlayerAnimationController(this, playerTexture);
 
         PolygonShape shape = new PolygonShape();
         shape.setAsBox(FEET_WIDTH/2f, FEET_HEIGHT/2f);
 
-//        placementSound = Gdx.audio.newSound(Gdx.files.internal("sounds/placeBlockSound.wav"));
-//        qCollectSound = Gdx.audio.newSound(Gdx.files.internal("sounds/qSound.wav"));
-//        walkingSound = Gdx.audio.newSound(Gdx.files.internal("sounds/running_Sound.wav"));
-//       walkingOnIce = Gdx.audio.newMusic(Gdx.files.internal("sounds/ice_Sound.wav"));
-//        bounceSound  = Gdx.audio.newSound(Gdx.files.internal("sounds/bounce_sound.mp3"));
-//        voidSound = Gdx.audio.newSound(Gdx.files.internal("sounds/wilhelm.wav"));
-//        deathSound = Gdx.audio.newSound(Gdx.files.internal("sounds/death_Sound.mp3"));
-//        pickupSound = Gdx.audio.newSound(Gdx.files.internal("Sounds/pickup dak.wav"));
-
         this.world = world;
-        FixtureDef feetDef = new FixtureDef();
-        feetDef.shape = shape;
-        feetDef.density = 0;
-        feetDef.friction = 1;
-        feetDef.restitution = 0;
-        feetDef.isSensor = false;
-        feet = new GameObject(world, new Vector2(pos.x, pos.y + FEET_Y_OFFSET), createPlayerBodyDef(), feetDef).getBody();
-        feet.setUserData(ContactManager.FEET);
-        feet.setGravityScale(0);
         body.setLinearDamping(0);
         body.setUserData(this);
 
-        podPosition = new Vector2(pos);
         deathCounter=0;
     }
 
-    public void render(GameMap map, Vector3 mousePos, KeyboardController controller, SpriteBatch batch, float delta) {
+    public void render(GameMap map, Vector3 mousePos, InputController controller, SpriteBatch batch, float delta) {
         this.map = map;
-        batch.draw(playerShip, podPosition.x, podPosition.y, 4, 4);
-
-
-        //adds Player spawned tiles to the array
-        if (queue.size()>0) playerPlacedTiles.addAll(queue);
-        queue.removeAll(queue);
-
-        for(RenderObject renderableObject : playerPlacedTiles)
-                renderableObject.render(batch, delta);
-
         timePassed += delta;
 
 
-        if (isPodLanding) {
-            spacePodLanding();
-        } else {
-            if (!isDead) {
+
                 movement(map, controller, batch, delta);
-                qCollect(controller, map);
-                blockPlacing(batch, map, controller, mousePos);
-            } else {
                 resolveDeath(batch, delta);
-            }
 
             super.render(batch, delta);
-        }
     }
 
 
 
 
-    private void movement(GameMap map, KeyboardController controller, SpriteBatch batch, float delta) {
+    private void movement(GameMap map, InputController controller, SpriteBatch batch, float delta) {
         Vector2 vel = body.getLinearVelocity();
 
-        isMidAir = !(ContactManager.feetCollisions > 0 && Math.abs(vel.y) <= 1e-2);
+//        isMidAir = !(ContactManager.feetCollisions > 0 && Math.abs(vel.y) <= 1e-2);
 
         // Jump
         if (!isMidAir) hasJumped = false;
@@ -160,11 +98,10 @@ public class Player extends TextureObject {
         // Movement
         int dir = controller.d ? 1 : controller.a ? -1 : 0;
         lookingDir = dir == 0 ? lookingDir : dir;
-        if (onIce && !isMidAir){
+        if (!isMidAir){
             //Sliding on ice
-            if (!walkingOnIce.isPlaying())walkingOnIce.play();
                body.applyForceToCenter(WALK_SPEED*dir,0,true);
-        }else if (!onIce && !isMidAir) {
+        }else if (!isMidAir) {
             // Grounded
 
             vel.x = WALK_SPEED * dir;
@@ -174,7 +111,6 @@ public class Player extends TextureObject {
             vel.add(AIR_WALK_FORCE * dir, 0);
         }
 
-        isCrouched = controller.s;
 
         // Restrict vel x
         vel.x = Math.min(Math.max(-MAX_X_VEL, vel.x), MAX_X_VEL);
@@ -185,34 +121,22 @@ public class Player extends TextureObject {
         // Move feet
         feet.setTransform(new Vector2(body.getPosition()).add(0, FEET_Y_OFFSET), 0);
         feet.setLinearVelocity(vel);
-        gunPosition.set(body.getPosition()).add(lookingDir * GUN_X_OFFSET, 0).add(-0.45f, -0.65f);
 
-        texture = animController.getTexture(dir, isMidAir, delta);
+//        texture = animController.getTexture(dir, isMidAir, delta);
     }
 
 
     public void resolveDeath(SpriteBatch batch, float delta) {
-        deathTimer += delta;
-        if (deathTimer < DEATH_TIMER_RED) batch.setColor(1, 0, 0, 1);
-        else if (deathTimer < RESPAWN_TIMER) batch.setColor(0, 0, 0, 1);
-        else {
             body.setTransform(spawnPosition, 0);
             isDead = false;
             batch.setColor(1, 1, 1, 1);
-        }
     }
 
     public void kill() {
         if (!isDead) {
             deathCounter++;
-            if (map.isPlayerOutOfBounds()){
-                voidSound.play();
-            }else {
-                deathSound.play();
-            }
 
             isDead = true;
-            deathTimer = 0;
         }
     }
 
@@ -220,13 +144,6 @@ public class Player extends TextureObject {
          return body.getPosition();
     }
 
-    public ArrayList<Tile> getPlayerPlacedTiles() {
-        return playerPlacedTiles;
-    }
-
-    public ArrayList<Tile> getQueue() {
-        return queue;
-    }
 
     /** The default fixturedef for players */
     private static FixtureDef createPlayerFixtureDef(){
@@ -262,25 +179,14 @@ public class Player extends TextureObject {
         return bodyDef;
     }
 
-    public void slide(boolean b) {
-        onIce = b;
-    }
 
-    public boolean isCrouched() {
-        return isCrouched;
-    }
 
     public void bounce(Body other) {
-        if (!isCrouched) {
             if (Math.round(other.getPosition().x) == Math.round(body.getPosition().x) && body.getLinearVelocity().y < 1)
-                bounceSound.stop();
-                bounceSound.play();
                 body.setLinearVelocity(body.getLinearVelocity().x, -body.getLinearVelocity().y);
-        }
     }
 
     public void playPickupSound() {
-        pickupSound.play();
     }
 
     public int getDeathCounter() {

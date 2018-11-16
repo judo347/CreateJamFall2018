557aefdb3262ae213f18126280b244b2ec6451d2
Mikkel K <32062449+judo347@users.noreply.github.com>
2018-11-16 19:55:39 +0100

    base setup (MainGame, Screen, GameMap) (mostly only creation of files)

--- GameFolder/core/src/dk/amminiti/helpers/GameInfo.java
+++ GameFolder/core/src/dk/amminiti/helpers/GameInfo.java
@@ -4,4 +4,7 @@ public class GameInfo {
 
     public static final int TILE_SIZE = 40;
     public static final float PPM = 1f/TILE_SIZE; //100px = 1m //PPM pixels per min //THIS ONE HAS TO BE A FLOAT!
+    public static final int SCREEN_WIDTH = 1280;
+    public static final int SCREEN_HEIGHT = 800;
+    public static final float ZOOM = PPM/3f;
 }

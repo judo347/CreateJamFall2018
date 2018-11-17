049ded63fa11bd9d75715ea2fe1b7a2884ccea48
NicEastvillage <nicoesterby@gmail.com>
2018-11-17 13:30:07 +0100

    Made map box into a floor

--- GameFolder/core/src/dk/amminiti/helpers/GameInfo.java
+++ GameFolder/core/src/dk/amminiti/helpers/GameInfo.java
@@ -6,6 +6,5 @@ public class GameInfo {
     public static final float PPM = 1f/TILE_SIZE; //100px = 1m //PPM pixels per min //THIS ONE HAS TO BE A FLOAT!
     public static final int SCREEN_WIDTH = 1400;
     public static final int SCREEN_HEIGHT = 1080;
-    public static final int MAPSIZE_WIDTH = 1280;
-    public static final int MAPSIZE_HEIGHT = 800;
+    public static final int MAPSIZE_WIDTH = 3000;
 }

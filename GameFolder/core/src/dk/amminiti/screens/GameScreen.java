b5aeac768836987889121bbd8b8db142e1448ca5
NicEastvillage <nicoesterby@gmail.com>
2018-11-17 10:21:27 +0100

    Added a camera that follows players.

--- GameFolder/core/src/dk/amminiti/screens/GameScreen.java
+++ GameFolder/core/src/dk/amminiti/screens/GameScreen.java
@@ -28,7 +28,7 @@ public class GameScreen implements Screen {
 
     private MainGame game;
     private SpriteBatch spriteBatch;
-    private OrthographicCamera camera;
+    private OrthographicTargetedCamera camera;
     private Stage stage;
     private GameMap gameMap;
     private InputController inputs = new InputController();
@@ -53,10 +53,7 @@ public class GameScreen implements Screen {
         this.spriteBatch = new SpriteBatch();
 
         //Position of the camera
-        this.camera = new OrthographicCamera(GameInfo.SCREEN_WIDTH, GameInfo.SCREEN_HEIGHT);
-        this.camera.zoom = GameInfo.ZOOM;
-        //camera.position.y = 500;
-        camera.position.y = camera.position.y + 8;
+        this.camera = new OrthographicTargetedCamera(GameInfo.SCREEN_WIDTH, GameInfo.SCREEN_HEIGHT);
         this.camera.update();
         this.debugRenderer = new Box2DDebugRenderer();
         gameObjectsListforRender = new ArrayList<TextureObject>();
@@ -74,11 +71,9 @@ public class GameScreen implements Screen {
         gameObjectsListforRender.add(fooster);
         gameObjectsListforRender.add(redcow);
 
+        camera.targets.add(p1);
+        camera.targets.add(p2);
 
-
-
-
-        //TODO Stage MAYBE
         spriteBatch.setProjectionMatrix(camera.combined);
     }
 
@@ -89,22 +84,21 @@ public class GameScreen implements Screen {
 
     @Override
     public void render(float delta) {
-        camera.update();
 
         world.step(delta, 3, 3);
         Gdx.gl.glClearColor(0f, 0.0f, 0f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         spriteBatch.begin();
-//        p1.render(spriteBatch, delta);
 
         for (TextureObject to:gameObjectsListforRender) {
             to.render(spriteBatch,delta);
         }
 
-
-        gameMap.render(spriteBatch, delta); //TODO
-        //TODO STAGE THINGS
+        gameMap.render(spriteBatch, delta);
         spriteBatch.end();
+
+        camera.update();
+        spriteBatch.setProjectionMatrix(camera.combined);
         debugRenderer.render(world, camera.combined);
     }
 
---
+++ GameFolder/core/src/dk/amminiti/screens/OrthographicTargetedCamera.java
@@ -0,0 +1,39 @@
+package dk.amminiti.screens;
+
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.math.Vector2;
+import dk.amminiti.entity.Player;
+import dk.amminiti.helpers.GameInfo;
+
+import java.util.ArrayList;
+
+public class OrthographicTargetedCamera extends OrthographicCamera {
+
+    private static final float SPEED = 0.15f; // Opposite of smooth movement. 1 = instant, 0 = no movement
+    private static final float ZOOM = GameInfo.PPM/1.2f;
+
+    public final ArrayList<Player> targets = new ArrayList<Player>();
+    private static final Vector2 ARENA_CENTER = new Vector2(0, 6);
+
+    public OrthographicTargetedCamera(float viewportWidth, float viewportHeight) {
+        super(viewportWidth, viewportHeight);
+        zoom = ZOOM;
+    }
+
+    @Override
+    public void update() {
+        Vector2 targetPos = new Vector2(ARENA_CENTER);
+        if (targets != null && targets.size() != 0) {
+            for (Player target : targets) {
+                targetPos.add(target.getBodyPos());
+            }
+            targetPos.scl(1f / (targets.size() + 1));
+        }
+
+        Vector2 diff = new Vector2(targetPos.x - position.x, targetPos.y - position.y);
+        position.x += diff.x * SPEED;
+        position.y += diff.y * SPEED;
+
+        super.update();
+    }
+}
